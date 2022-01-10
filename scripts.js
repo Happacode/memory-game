c75effce961ef7a55e0438d20d5df8e2b9c03e98
Happacode <happacode@protonmail.com>
2022-01-10 13:46:23 -0800

    Finish match card condition execution

--- scripts.js
+++ scripts.js
@@ -9,7 +9,38 @@ function flipCard() {
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    return;
   }
+
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    disableCards();
+    return;
+  }
+
+  unflipCards();
+}
+
+// ternary operator matching condition
+// let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+// isMatch ? disableCards() : unflipCards();
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
